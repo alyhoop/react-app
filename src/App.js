580094c0588f7ef6de7e0520dd4bf8dcedfe055f
Alyssa Hooper <nesteralyssa@gmail.com>
2020-04-13 13:51:16 -0700

    tweet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Tweet from './tweet';
+
 
 
 function App() {
+
+
   return (
-    <div>
+    <div className="app">
       <h1>Hello React</h1>
+      <Tweet />
     </div>
   );
 }
