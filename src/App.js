115d10c55b92d3c18f0da0108c02590d050acbfc
Alyssa Hooper <nesteralyssa@gmail.com>
2020-04-14 15:14:35 -0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React, {useState} from 'react';
-import Tweet from './tweet';
+import React, {useEffect, useState} from 'react';
+import './App.css';
 
 
+const App = () => {
+  const APP_ID = 'a4075a01';
+  const APP_KEY = '5c95ebf648ed40dd5bbeb6c011b04a02';
+  const exampleReq =
+`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-function App() {
+const [counter, setCounter] = useState(0);
 
-  const [users, setUsers] = useState([
-      {name: 'Aly', message: 'hiii'},
-      {name: 'm00p', message: 'hey'}
-  ]);
+useEffect(() => {
+  console.log('Effect has been run.');
+}, []);
 
   return (
     <div className="app">
-      {users.map(user => (
-        <Tweet name={user.name} message={user.message}/>
-      ))}
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">
+        Search
+        </button>
+      </form>
     </div>
   );
 }
