f00eea8b6481af667dfba4c7721ac8e7ef32ee32
Alyssa Hooper <nesteralyssa@gmail.com>
2020-04-14 16:17:58 -0700

    picture redering

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from "./Recipe";
 
 
 const App = () => {
   const APP_ID = 'a4075a01';
   const APP_KEY = '5c95ebf648ed40dd5bbeb6c011b04a02';
 
+  const [recipes, setRecipes] = useState([]);
 
 useEffect( () => {
-
+  getRecipes();
 }, []);
 
 const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
+    console.log(data.hits);
 }
 
   return (
@@ -25,8 +28,16 @@ const getRecipes = async () => {
         Search
         </button>
       </form>
+      {recipes.map(recipe => (
+          <Recipe
+          key={recipe.recipe.label}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
